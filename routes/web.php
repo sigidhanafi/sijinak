2f35fc0034126626590c6ab18809aa7f1fbaec17
gafnaa <gafnaalfaatiha@gmail.com>
2025-05-18 23:02:42 +0700

    fix: route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\ActivitiesController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IzinSiswaController;
@@ -21,7 +20,6 @@ Route::get('/status-izin', [ActivitiesController::class, 'status'])->name('activ
 Route::get('/validasi-izin', [IzinSiswaController::class, 'index'])->name('activities.validasi-izin');
 
 Route::post('/guru-piket/izin-siswa/{id}/approve', [IzinSiswaController::class, 'approve'])->name('permission.approve');
-
 Route::post('/guru-piket/izin-siswa/{id}/reject', [IzinSiswaController::class, 'reject'])->name('permission.reject');
 
 
