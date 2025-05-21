b58d83b840c3d9638e26ef2f4b20bdcdf8247e19
shampoerna <153973086+shampoerna@users.noreply.github.com>
2025-05-21 16:28:43 +0700

    fix: avatar path

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -291,7 +291,7 @@
         <li class="nav-item navbar-dropdown dropdown-user dropdown">
           <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
             <div class="avatar avatar-online">
-              <img src="../../assets/img/avatars/1.png" alt class="rounded-circle" />
+              <img src="/assets/img/avatars/1.png" alt class="rounded-circle" />
             </div>
           </a>
           <ul class="dropdown-menu dropdown-menu-end">
@@ -300,7 +300,7 @@
                 <div class="d-flex">
                   <div class="flex-shrink-0 me-3">
                     <div class="avatar avatar-online">
-                      <img src="../../assets/img/avatars/1.png" alt class="rounded-circle" />
+                      <img src="/assets/img/avatars/1.png" alt class="rounded-circle" />
                     </div>
                   </div>
                   <div class="flex-grow-1">
